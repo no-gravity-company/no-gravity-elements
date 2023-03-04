b457d06e89b49dfb04cd1a7fdcdce1cb37d8d7e3
Adrian Ramiro <sacebado@gmail.com>
2023-03-05 00:11:20 +0100

    NGE-012: Add indent and eol-last to linting

--- packages/components/atoms/Counter/Counter.stories.tsx
+++ packages/components/atoms/Counter/Counter.stories.tsx
@@ -1,14 +1,14 @@
 import { h } from 'preact';
 
 export default {
-  title: 'My Counterr',
-  parameters: {
-    // Specify that the component is a Web Component
-    webComponents: {
-      // Set the tag name of the Web Component
-      tagName: 'my-counter',
-    },
-  },
+	title: 'My Counterr',
+	parameters: {
+		// Specify that the component is a Web Component
+		webComponents: {
+			// Set the tag name of the Web Component
+			tagName: 'my-counter',
+		},
+	},
 };
 
 export const Default = () => <my-counter />;
--- packages/components/molecules/Counter/Counter.stories.tsx
+++ packages/components/molecules/Counter/Counter.stories.tsx
@@ -2,14 +2,14 @@ import { h } from 'preact';
 import './lib/index';
 
 export default {
-  title: 'My Counter Molecules',
-  parameters: {
-    // Specify that the component is a Web Component
-    webComponents: {
-      // Set the tag name of the Web Component
-      tagName: 'my-counter',
-    },
-  },
+	title: 'My Counter Molecules',
+	parameters: {
+		// Specify that the component is a Web Component
+		webComponents: {
+			// Set the tag name of the Web Component
+			tagName: 'my-counter',
+		},
+	},
 };
 
 export const DefaultMolecules = () => <my-counter />;
