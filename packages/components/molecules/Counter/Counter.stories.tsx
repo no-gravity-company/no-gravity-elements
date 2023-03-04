4d96b6511f06f5c853a8af673dc9faabd4ee6964
Adrian Ramiro <sacebado@gmail.com>
2023-03-04 05:16:57 +0100

    NGE-007: Add storybook

---
+++ packages/components/molecules/Counter/Counter.stories.tsx
@@ -0,0 +1,15 @@
+import { h } from 'preact';
+import './lib/Counter';
+
+export default {
+  title: 'My Counter Molecules',
+  parameters: {
+    // Specify that the component is a Web Component
+    webComponents: {
+      // Set the tag name of the Web Component
+      tagName: 'my-counter',
+    },
+  },
+};
+
+export const DefaultMolecules = () => <my-counter />;
