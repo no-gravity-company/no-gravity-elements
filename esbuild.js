2bbb01c7a97f07d39ce43a38bf5d5738517c4e3e
Adrian Ramiro <sacebado@gmail.com>
2023-03-04 05:16:57 +0100

    NGE-007: Add storybook

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,21 @@
 const esbuild = require('esbuild');
 const glob = require('tiny-glob');
 (async () => {
-    const tsxPaths = await glob('packages/components/**/*.tsx');
-    const componentPaths = tsxPaths.filter(filePath => !filePath.includes('.spec.tsx'));
-    esbuild.build({
-        entryNames: '[dir]/lib/[name]',
-        entryPoints: componentPaths,
-        bundle: true,
-        minify: true,
-        outdir: 'packages/components/',
-        sourcemap: true,
-      }).catch(() => process.exit(1));
+  const tsxPaths = await glob('packages/components/**/*.tsx');
+  const componentPaths = tsxPaths.filter(
+    (filePath) =>
+      !filePath.includes('.spec.tsx') && !filePath.includes('.stories.tsx')
+  );
+  esbuild
+    .build({
+      entryNames: '[dir]/lib/[name]',
+      entryPoints: componentPaths,
+      bundle: true,
+      minify: true,
+      outdir: 'packages/components/',
+      sourcemap: true,
+      platform: 'browser',
+      target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    })
+    .catch(() => process.exit(1));
 })();
---
+++ typings/jsx.d.ts
@@ -0,0 +1,12 @@
+// jsx.d.ts
+import { Attributes } from 'preact';
+
+declare module 'preact/src/jsx' {
+  namespace JSXInternal {
+      // We're extending the IntrinsicElements interface which holds a kv-list of
+      // available html-tags.
+      interface IntrinsicElements {
+        [elemName: string]: Attributes & { [propName: string]: any };
+      }
+  }
+}
