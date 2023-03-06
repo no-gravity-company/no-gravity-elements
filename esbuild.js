198b5d3d5b73734b632e8dd815f699496f9e9df3
Adrian Ramiro <sacebado@gmail.com>
2023-03-06 22:36:43 +0100

    NGE-019: Add watcher scrpts and change esbuild config

--- esbuild.js
+++ esbuild.js
@@ -1,16 +1,41 @@
 const esbuild = require('esbuild');
+const chokidar = require('chokidar');
 const glob = require('tiny-glob');
+
+const getCommonOps = (componentPaths) => {
+    const ops = {
+        entryNames: '[dir]/lib/index',
+        entryPoints: componentPaths,
+        bundle: true,
+        minify: true,
+        outdir: 'packages/components/',
+        sourcemap: true,
+        platform: 'browser',
+        target: 'esnext',
+        format: 'esm',
+        allowOverwrite: true,
+    };
+    return ops;
+};
+
 (async () => {
     const componentPaths = await glob('packages/components/**/register.ts');
-    esbuild
-        .build({
-            entryNames: '[dir]/lib/index',
-            entryPoints: componentPaths,
-            bundle: true,
-            minify: true,
-            outdir: 'packages/components/',
-            sourcemap: true,
-            platform: 'browser',
-        })
-        .catch(() => process.exit(1));
+    const buildOps = getCommonOps(componentPaths);
+    await esbuild.build(buildOps);
+
+    if (process.argv.includes('--watch')) {
+        const watcher = chokidar.watch([
+            'packages/components/**/*.{ts,tsx}',
+            '!packages/components/**/*.{spec,stories}.*',
+        ]);
+        watcher.on('change', async (path) => {
+            console.log(`Detected change in ${path}, rebuilding...`);
+            try {
+                await esbuild.build(buildOps);
+                console.log(`Rebuild of ${path} succeeded!`);
+            } catch (error) {
+                console.error(`Rebuild of ${path} failed:`, error);
+            }
+        });
+    }
 })();
