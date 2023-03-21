fbe6b0375a739a693070cfa05eeea0f8c32ee8ad
Jack Blakeston <jackblakeston93@gmail.com>
2023-03-21 23:16:54 +0100

    NGE-014: Improve linting rules

--- packages/components/atoms/DisplayBox/DisplayBox.tsx
+++ packages/components/atoms/DisplayBox/DisplayBox.tsx
@@ -2,22 +2,22 @@ import { Fragment, FunctionComponent, h } from 'preact';
 import { DisplayBoxProps } from './types';
 
 const DisplayBox: FunctionComponent<DisplayBoxProps> = ({
-    label = '',
-    value = 0,
+  label = '',
+  value = 0,
 }: DisplayBoxProps) => {
-    const handleClick = () => {
-        console.log('clicked');
-    };
+  const handleClick = () => {
+    console.log('clicked');
+  };
 
-    return (
-        <div>
-            <span>DISPLAY BOX</span>
-            <span>{label}</span>
-            <span>value</span>
-            <span>{value}</span>
-            <button onClick={handleClick}></button>
-        </div>
-    );
+  return (
+    <div>
+      <span>DISPLAY BOX</span>
+      <span>{label}</span>
+      <span>value</span>
+      <span>{value}</span>
+      <button onClick={handleClick}></button>
+    </div>
+  );
 };
 
 export default DisplayBox;
--- packages/components/atoms/InfoBox/InfoBox.tsx
+++ packages/components/atoms/InfoBox/InfoBox.tsx
@@ -6,35 +6,38 @@ import { InfoBoxProps } from './types';
 const inputValue = signal('');
 
 const InfoBox: FunctionComponent<InfoBoxProps> = ({ label }: InfoBoxProps) => {
-    const handleInputChange = (event: Event) => {
-        const inputElement = event.target as HTMLInputElement;
-        inputValue.value = inputElement.value;
-    };
+  const handleInputChange = (event: Event) => {
+    const inputElement = event.target as HTMLInputElement;
+    inputValue.value = inputElement.value;
+  };
 
-    const handleDivClick = (event: Event) => {
-        // TODO extract to function in tools repo -> publish(eventName, payload, element)
-        if (event && event.target) {
-            const customEvent = new CustomEvent('ButtonClick', {
-                detail: {
-                    'nge-event': true,
-                },
-                // TODO try to consume it without those attrs (maybe they are only needed if we want to cacth them with a listener)
-                bubbles: true,
-                composed: true,
-            });
-            event.target.dispatchEvent(customEvent);
-        }
-    };
+  console.log('wha');
+  const a = 0;
 
-    return (
-        <Fragment>
-            <span className="main">HEY</span>
-            <span>{label}</span>
-            <input type="text" value={inputValue} onInput={handleInputChange} />
-            <span>{inputValue}</span>
-            <button onClick={handleDivClick}></button>
-        </Fragment>
-    );
+  const handleDivClick = (event: Event) => {
+    // TODO extract to function in tools repo -> publish(eventName, payload, element)
+    if (event && event.target) {
+      const customEvent = new CustomEvent('ButtonClick', {
+        detail: {
+          'nge-event': true,
+        },
+        // TODO try to consume it without those attrs (maybe they are only needed if we want to cacth them with a listener)
+        bubbles: true,
+        composed: true,
+      });
+      event.target.dispatchEvent(customEvent);
+    }
+  };
+
+  return (
+    <Fragment>
+      <span className='main'>HEY</span>
+      <span>{label}</span>
+      <input type='text' value={inputValue} onInput={handleInputChange} />
+      <span>{inputValue}</span>
+      <button onClick={handleDivClick}></button>
+    </Fragment>
+  );
 };
 
 export default InfoBox;
