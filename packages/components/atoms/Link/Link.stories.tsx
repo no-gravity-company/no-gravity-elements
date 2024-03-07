42fe615c64d90831a778a9a7112eb796ef0ee034
Adrian Ramiro <sacebado@gmail.com>
2024-03-08 00:31:37 +0100

    NGE-068: Link component

---
+++ packages/components/atoms/Link/Link.stories.tsx
@@ -0,0 +1,53 @@
+import { Story } from '@storybook/preact';
+import { html } from 'lit-html';
+import { TypographySizes } from 'packages/types';
+
+import { LinkProps } from '@atoms/Link/types';
+
+import '@atoms/Link/lib/index';
+
+export default {
+  title: 'Link',
+  component: 'nge-link',
+  parameters: {
+    webComponents: {
+      tagName: 'nge-link',
+    },
+  },
+  argTypes: {
+    text: {
+      description: 'Link text',
+      control: { type: 'text' },
+      table: {
+        defaultValue: { summary: 'string' },
+      },
+    },
+    size: {
+      description: 'Text size',
+      control: { type: 'text' },
+      table: {
+        defaultValue: { summary: 'h6' },
+      },
+    },
+    href: {
+      description: 'Link url',
+      control: { type: 'text' },
+      table: {
+        defaultValue: { summary: 'primary' },
+      },
+    },
+  },
+};
+type StoryLinkProps = LinkProps & { text: string };
+
+export const Default: Story<StoryLinkProps> = (args): any => {
+  const { text, href, size } = args;
+
+  return html` <nge-link size="${size}" href="${href}">${text}</nge-link> `;
+};
+
+Default.args = {
+  href: '/',
+  text: 'Link',
+  size: TypographySizes.h6,
+};
