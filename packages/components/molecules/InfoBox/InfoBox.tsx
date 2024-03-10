e6f94ae180681f8d0b979462d072602acf2e0ac9
Jack Blakeston <jackblakeston93@gmail.com>
2024-03-10 16:44:06 +0100

    NGE-076: Fix checkout action not fetching entire commit history
    Set to fetch entire commit history so lerna can version correctly

--- packages/components/molecules/InfoBox/InfoBox.tsx
+++ packages/components/molecules/InfoBox/InfoBox.tsx
@@ -26,6 +26,7 @@ const InfoBox: FunctionComponent<InfoBoxProps> = ({ label }: InfoBoxProps) => {
     const inputElement = event.target as HTMLInputElement;
     inputValue.value = inputElement.value;
   };
+  // TODO delete this comment
 
   const handleDivClick = (event: Event) => {
     // TODO extract to function in tools repo -> publish(eventName, payload, element)
