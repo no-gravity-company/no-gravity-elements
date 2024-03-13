1734cd35fd68d33b54ca5642745b9f099610a224
Adrian Ramiro <sacebado@gmail.com>
2024-03-13 18:51:55 +0100

    chore: update properties

--- packages/components/atoms/Icon/Icon.tsx
+++ packages/components/atoms/Icon/Icon.tsx
@@ -6,6 +6,20 @@ import useFetchSVG from '@hooks/useFetchSVG';
 
 import { IconSizes } from '@types';
 
+/**
+ * <nge-icon>
+ *
+ *  Icon component
+ *
+ * @element nge-icon
+ *
+ * @prop {'string'} [name] - Icon name
+ * @prop {'extra-small'|'small'|'medium'|'regular'|'large'|'extra-large'} [size] - Icon size
+ *
+ * @example
+ * <nge-icon name="cross"></nge-icon>
+ */
+
 const Icon: FunctionComponent<IconProps> = ({ name, size }: IconProps) => {
   const svgComponent = useFetchSVG(name);
 
