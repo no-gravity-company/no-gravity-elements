47a67d0511af9e5d90bc3bf1a9f58cda6c25b779
Adrian Ramiro <sacebado@gmail.com>
2024-12-19 23:15:55 +0100

    fix: change button classnames

--- packages/components/atoms/Button/Button.tsx
+++ packages/components/atoms/Button/Button.tsx
@@ -65,9 +65,9 @@ const Button: FunctionComponent<ButtonProps> = ({
   const content = (
     <>
       <nge-icon className='loading-icon' name={IconNames.bouncingCircles} size={IconSizes.medium} />
-      {icon && iconside === 'left' &&  <nge-icon className='content' name={icon} size={IconSizes.small} />}
-      <span className='content'>{value}</span>
-      {icon && iconside === 'right' && <nge-icon className='content' name={icon} size={IconSizes.small} />}
+      {icon && iconside === 'left' &&  <nge-icon className='button-left-icon' name={icon} size={IconSizes.small} />}
+      <span className='button-text'>{value}</span>
+      {icon && iconside === 'right' && <nge-icon className='button-right-icon' name={icon} size={IconSizes.small} />}
     </>
   );
 
