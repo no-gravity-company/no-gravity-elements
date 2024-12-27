c2dbf4270a1aca118bfa7cceb07587321895b999
Adrian Ramiro <sacebado@gmail.com>
2024-12-27 10:27:24 +0100

    fix: show mobile with JS logic

--- packages/components/atoms/Header/Header.tsx
+++ packages/components/atoms/Header/Header.tsx
@@ -36,11 +36,20 @@ import './Header.modules.scss';
 
 const Header: FunctionComponent = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkIfMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkIfMobile();
+    window.addEventListener('resize', checkIfMobile);
+    return () => window.removeEventListener('resize', checkIfMobile);
+  }, []);
 
   const toggleMenu = () => {
     messageBus.publish(MessageBusEvents.HEADER_MENU_ICON, { show: !isMenuOpen })
   };
-
 
   useEffect(() => {
     const eventId = messageBus.subscribe(MessageBusEvents.HEADER_MENU_ICON, (data) => {
@@ -51,7 +60,6 @@ const Header: FunctionComponent = () => {
     }
   }, []);
 
-
   return (
     <Fragment>
       <div className='nge-header'>
@@ -59,59 +67,65 @@ const Header: FunctionComponent = () => {
           <slot name='logo' />
         </div>
 
-        <nav className='navigation desktop-nav'>
-          <slot name='navigation' />
-        </nav>
+        {!isMobile && (
+          <nav className='navigation desktop-nav'>
+            <slot name='navigation' />
+          </nav>
+        )}
 
         <div className='options'>
           <slot name='options' />
         </div>
 
-        <button
-          className={classNames('burger-menu', { active: isMenuOpen })}
-          onClick={toggleMenu}
-          aria-label='Toggle menu'
-        >
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='40'
-            height='40'
-            viewBox='0 0 200 200'
-          >
-            <g stroke-width='6.5' stroke-linecap='round'>
-              <path d='M72 82.286h28.75' fill='#000000' fill-rule='evenodd' stroke='#000000' />
-              <path
-                d='M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554'
-                fill='none'
-                stroke='#000000'
-              />
-              <path d='M72 125.143h28.75' fill='#000000' fill-rule='evenodd' stroke='#000000' />
-              <path
-                d='M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554'
-                fill='none'
-                stroke='#000000'
-              />
-              <path
-                d='M100.75 82.286h28.75'
-                fill='#000000'
-                fill-rule='evenodd'
-                stroke='#000000'
-              />
-              <path
-                d='M100.75 125.143h28.75'
-                fill='#009100'
-                fill-rule='evenodd'
-                stroke='#000000'
-              />
-            </g>
-          </svg>
-        </button>
+        {isMobile && (
+          <>
+            <button
+              className={classNames('burger-menu', { active: isMenuOpen })}
+              onClick={toggleMenu}
+              aria-label='Toggle menu'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='40'
+                height='40'
+                viewBox='0 0 200 200'
+              >
+                <g stroke-width='6.5' stroke-linecap='round'>
+                  <path d='M72 82.286h28.75' fill='#000000' fill-rule='evenodd' stroke='#000000' />
+                  <path
+                    d='M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554'
+                    fill='none'
+                    stroke='#000000'
+                  />
+                  <path d='M72 125.143h28.75' fill='#000000' fill-rule='evenodd' stroke='#000000' />
+                  <path
+                    d='M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554'
+                    fill='none'
+                    stroke='#000000'
+                  />
+                  <path
+                    d='M100.75 82.286h28.75'
+                    fill='#000000'
+                    fill-rule='evenodd'
+                    stroke='#000000'
+                  />
+                  <path
+                    d='M100.75 125.143h28.75'
+                    fill='#009100'
+                    fill-rule='evenodd'
+                    stroke='#000000'
+                  />
+                </g>
+              </svg>
+            </button>
 
-        <aside className={classNames('side-menu', { open: isMenuOpen })}>
-          <div className='mobile-nav'>
-            <slot name='navigation' />
-          </div>
-        </aside>
+            <aside className={classNames('side-menu', { open: isMenuOpen })}>
+              <div className='mobile-nav'>
+                <slot name='navigation' />
+              </div>
+            </aside>
+          </>
+        )}
       </div>
     </Fragment>
   );
